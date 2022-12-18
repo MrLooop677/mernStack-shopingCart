da16c121330c96c4811d84cc4f50071f09eeb20a
eslam mahmoud <eslam.mahmoud@dtb.com>
2022-12-18 12:43:54 +0200

    HeaderComp

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,11 @@
-
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="layOut">
+      <Header />
+      <main>content</main>
+      <footer>Footer</footer>
     </div>
   );
 }
