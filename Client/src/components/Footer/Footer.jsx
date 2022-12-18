cdb5273ed8ad007a3914b7e029d9613e32d73309
eslam mahmoud <eslam.mahmoud@dtb.com>
2022-12-18 13:47:41 +0200

    style

--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "../../css/Footer/footer.css";
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      &copy; <strong>copyright </strong>
+      Eslam2022
+    </footer>
+  );
 };
 
 export default Footer;
